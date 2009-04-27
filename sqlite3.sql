d3d9c7f6b1c3af9cb150efa00411b93626044ef6
Chris Williams <chris@bingosnet.co.uk>
2009-04-27 16:23:44 +0100

    Added a SQLite based schema file

---
+++ sqlite3.sql
@@ -0,0 +1,28 @@
+CREATE TABLE BotChannels (
+  NickName varchar(15) NOT NULL PRIMARY KEY,
+  Channel varchar(200) NOT NULL
+);
+
+CREATE TABLE BotConfig (
+  NickName varchar(15) NOT NULL UNIQUE PRIMARY KEY,
+  IRCName varchar(50) default NULL,
+  IRCServer varchar(67) NOT NULL,
+  IRCPort int(5) default '6667',
+  IRCUser varchar(10) NOT NULL default 'binbot',
+  IRCUmode varchar(10) default 'i'
+);
+
+CREATE TABLE BotLogs (
+  TimeStamp bigint(20) NOT NULL default '0' PRIMARY KEY,
+  UniqID bigint(20) default NULL,
+  BotNick varchar(15) default NULL,
+  Channel varchar(200) default NULL,
+  Entry text
+);
+
+CREATE TABLE Trust (
+  BotNick varchar(19) default NULL,
+  Channel varchar(200) default NULL,
+  Identity varchar(100) default NULL,
+  Mode char(1) default NULL
+);
